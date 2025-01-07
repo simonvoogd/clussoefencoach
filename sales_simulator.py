4554e0385fcfd7051d8c9e209a6c2c57cb94ca14
simonvoogd <s.voogd@conceptsales.nl>
2025-01-07 13:51:02 +0100

    Update sales_simulator.py
    test8

--- sales_simulator.py
+++ sales_simulator.py
@@ -7,11 +7,12 @@ st.write(f"OpenAI library version: {openai.__version__}")
 # OpenAI API-sleutel instellen
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-st.title("CLUSS-oefencoach - minimale test7!")
+st.title("CLUSS-oefencoach - minimale test8!")
 
 # Maak een voorbeeldaanroep naar de API
 try:
-    response = openai.ChatCompletion.create(
+        #response = openai.ChatCompletion.create(
+        response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "Je bent een vriendelijke assistent."},
